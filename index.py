34fdc1937648dec5467891f4a3c077d32f02ddb5
xinru911017 <s1102292@gm.ppu.edu.tw>
2022-11-23 01:57:02 +0800

    version_2

--- index.py
+++ index.py
@@ -5,11 +5,11 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 from flask import Flask, render_template, request
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 app = Flask(__name__)
-
 
 @app.route("/hi")
 def course():
@@ -75,9 +75,25 @@ def movie():
 
     for item in result:
         picture = item.find("img").get("src").replace(" ", "")
-        title = item.find("div", class_="filetime").text
-        movie_id = item.find("div", class_="filetime").find("a").get("href").replace("/", "").replace("movie", "")
-        hyperlink = "http://www.atmovies.com.tw" + item.find("div", class_="filetime").find("a").get("href")
+        rate = item.find("div", class_="runtime").text
+        test_rate = item.find_all("img")
+        for img in test_rate:
+            if 'src' in img.attrs:
+                if img['src'].endswith('/images/cer_G.gif'):
+                    rate = "普遍級(一般觀眾皆可觀賞)"
+                elif img['src'].endswith('/images/cer_P.gif') :
+                    rate = "保護級(未滿六歲之兒童不得觀賞，六歲以上未滿十二歲之兒童須父母、師長或成年親友陪伴輔導觀賞)"
+                elif img['src'].endswith('/images/cer_F2.gif') :
+                    rate = "輔導級(未滿十二歲之兒童不得觀賞)"
+                elif img['src'].endswith('/images/cer_F5.gif') :
+                    rate = "輔導級(未滿十五歲之人不得觀賞)"
+                elif img['src'].endswith('/images/cer_R.gif'):
+                    rate = "限制級(未滿十八歲之人不得觀賞)"
+                else :
+                    rate = "尚無電影分級資訊"
+        title = item.find("div", class_="filmtitle").text
+        movie_id = item.find("div", class_="filmtitle").find("a").get("href").replace("/", "").replace("movie", "")
+        hyperlink = "http://www.atmovies.com.tw" + item.find("div", class_="filmtitle").find("a").get("href")
         show = item.find("div", class_="runtime").text.replace("上映日期：", "")
         show = show.replace("片長：", "")
         show = show.replace("分", "")
@@ -87,14 +103,16 @@ def movie():
         doc = {
             "title": title,
             "picture": picture,
+            "rate": rate,
             "hyperlink": hyperlink,
             "showDate": showDate,
             "showLength": showLength,
             "lastUpdate": lastUpdate
-         }
+        }
 
         doc_ref = db.collection("心如電影").document(movie_id)
         doc_ref.set(doc)
+
     return "近期上映電影已爬蟲及存檔完畢，網站最近更新日期為：" + lastUpdate 
 
 @app.route("/search_movie", methods=["POST","GET"])
@@ -103,27 +121,28 @@ def search_movie():
         MovieTitle = request.form["MovieTitle"]
         info = ""     
         collection_ref = db.collection("心如電影")
-        docs = collection_ref.order_by("showDate").get()    
+        docs = collection_ref.order_by("showDate").get()
         for doc in docs:
             if MovieTitle in doc.to_dict()["title"]: 
-                info += "片名：" + doc.to_dict()["title"] + "<br>" 
-                info += "海報：" + doc.to_dict()["picture"] + "<br>"
-                info += "影片介紹：" + doc.to_dict()["hyperlink"] + "<br>"
-                info += "片長：" + doc.to_dict()["showLength"] + " 分鐘<br>" 
-                info += "上映日期：" + doc.to_dict()["showDate"] + "<br><br>"           
+                info += "片名：<a target = _blank href=" + doc.to_dict()["hyperlink"] + ">" + doc.to_dict()["title"] + "</a>" + "<br>" 
+                info += "分級資訊：" + doc.to_dict()["rate"] + "<br><br>"
+        if info == "":
+            info += "查無此電影，<a href = http://www.atmovies.com.tw/movie/next/>前往官網</a>" 
         return info
+    else:  
+        return render_template("search_movie.html")
 
 @app.route("/")
 def index():
     homepage = "<h1>李心如 Python 網頁</h1>"
-    homepage += "<a href=/hi>Hi~</a><br>"
-    homepage += "<a href=/today>顯示日期時間</a><br>"
-    homepage += "<a href=/welcome?nick=xinru>傳送使用者暱稱</a><br>"
-    homepage += "<a href=/about>李心如簡介網頁</a><br>"
-    homepage += "<a href=/account>表單</a><br>"
-    homepage += "<br><a href=/search>選修課程查詢</a><br>"
-    homepage += "<br><a href=/movie>讀取開眼電影即將上映影片，寫入Firestore</a><br>"
-    homepage += "<br><a href=/search_movie>電影查詢</a><br>"
+    homepage += "<a href=/hi target = _blank>Hi~</a><br>"
+    homepage += "<a href=/today target = _blank>顯示日期時間</a><br>"
+    homepage += "<a href=/welcome?nick=xinru target = _blank>傳送使用者暱稱</a><br>"
+    homepage += "<a href=/about target = _blank>李心如簡介網頁</a><br>"
+    homepage += "<a href=/account target = _blank>表單</a><br>"
+    homepage += "<a href=/search target = _blank>選修課程查詢</a><br>"
+    homepage += "<br><a href=/movie target = _blank>讀取開眼電影即將上映影片，寫入Firestore</a><br>"
+    homepage += "<a href=/search_movie target = _blank>電影查詢</a><br>"
     return homepage
 
 # if __name__ == "__main__":
